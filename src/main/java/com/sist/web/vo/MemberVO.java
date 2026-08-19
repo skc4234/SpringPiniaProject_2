e46d90a6087c9a8a2359bdfe136a4482d3d224ec
skc4234 <skc4234@naver.com>
2026-08-19 15:08:43 +0900

    docker-compose settings

--- src/main/java/com/sist/web/vo/MemberVO.java
+++ src/main/java/com/sist/web/vo/MemberVO.java
@@ -1,4 +1,7 @@
 package com.sist.web.vo;
+
+import lombok.Data;
+
 /*
 USERID   NOT NULL VARCHAR2(20)  
 USERNAME NOT NULL VARCHAR2(50)  
@@ -6,6 +9,7 @@ USERPWD  NOT NULL VARCHAR2(300)
 ENABLE            NUMBER(1)     
 SEX               VARCHAR2(6)
  */
+@Data
 public class MemberVO {
 	private String userid,username,userpwd,sex;
 	private int enable;
